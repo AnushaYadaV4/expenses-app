4cb9692fa904982eaabf7a4d5b20f102417c62d7
AnushaYadaV4 <anu468mjl@gmail.com>
2023-05-16 09:38:15 +0530

    implementing sign up

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter } from "react-router-dom";
+
+//import ContextProvier from "./store/ContextProvider";
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+    
     <App />
+ 
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
